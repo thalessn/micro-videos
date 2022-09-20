a67ea1d6334ca16fd1dc71bf2d4054942ed4c7fd
Thales Nascimento <thales.silva.nascimento@gmail.com>
2022-09-20 19:53:55 -0300

    test: ensure category constructor receive correct values

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,8 +1,56 @@
 import { Category } from "./category";
+import { omit } from "lodash";
 
-describe("Category Tests", () => {
-  test("Constructo of category", () => {
-    const category = new Category("Movie");
-    expect(category.name).toBe("Movie");
+describe("Category Unit Tests", () => {
+  test("Constructor of category", () => {
+    let category = new Category({ name: "Movie" });
+    let props = omit(category.props, "created_at");
+    expect(props).toStrictEqual({
+      name: "Movie",
+      description: null,
+      is_active: true,
+    });
+    expect(category.props.created_at).toBeInstanceOf(Date);
+
+    let created_at = new Date();
+    category = new Category({
+      name: "Movie",
+      description: "some description",
+      is_active: false,
+    });
+    expect(category.props).toStrictEqual({
+      name: "Movie",
+      description: "some description",
+      is_active: false,
+      created_at,
+    });
+
+    category = new Category({
+      name: "Movie",
+      description: "some description 3",
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      description: "some description 3",
+    });
+
+    category = new Category({
+      name: "Movie",
+      is_active: true,
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      is_active: true,
+    });
+
+    created_at = new Date();
+    category = new Category({
+      name: "Movie",
+      created_at,
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      created_at,
+    });
   });
 });
